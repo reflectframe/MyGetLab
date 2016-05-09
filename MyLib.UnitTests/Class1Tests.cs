486ba4b9959597e35a9362c6a548088c9bf4377b
Mikael Ivarsson <micke@reflectframe.com>
2016-05-09 20:32:55 +0200

    Added simple project with NUnit test.

---
+++ MyLib.UnitTests/Class1Tests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLib.UnitTests
+{
+    public class Class1Tests
+    {
+        [Test]
+        public void CanCreateInstance()
+        {
+            var target = new Class1();
+            Assert.IsInstanceOf<Class1>(target);
+        }
+    }
+}
